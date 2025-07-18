d4870ef2f663d7109150fec20ef2314ac892ad99
Pavitraj-Singh <159785211+Pavitraj-Singh@users.noreply.github.com>
2025-07-18 15:05:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,70 @@
+# Import necessary libraries
 import pickle
 import streamlit as st
 import requests
 
+# Function to fetch poster image from TMDB API
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8228730e3316a282cbe0d287b58da186&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8228730e3316a282cbe0d287b58da186&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+    poster_path = data.get('poster_path')  # Use .get() to avoid KeyError if missing
+
+    # Handle case when poster is not available
+    if poster_path:
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    else:
+        full_path = "https://via.placeholder.com/500x750?text=No+Poster"
+
     return full_path
 
+# Function to recommend top 5 similar movies
 def recommend(movie):
+    # Get index of the selected movie
     index = movies[movies['title'] == movie].index[0]
+
+    # Sort all movies based on similarity score
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
     recommended_movie_names = []
     recommended_movie_posters = []
+
+    # Get top 5 recommended movies (excluding the selected one)
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movie_posters.append(fetch_poster(movie_id))
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
+# --- Streamlit Frontend ---
+st.header('🎬 Movie Recommender System')
 
-st.header('Movie Recommender System')
-movies = pickle.load(open(r"C:\Users\pavit\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.13\Movie Recommendation System\movie_list.pkl",'rb'))
-similarity = pickle.load(open(r"C:\Users\pavit\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.13\Movie Recommendation System\similarity.pkl",'rb'))
+# Load movie data and similarity matrix from relative path
+# These files must be in the same directory as this script
+movies = pickle.load(open("movie_list.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
+# Get list of all movie titles
 movie_list = movies['title'].values
+
+# Create dropdown for user to select a movie
 selected_movie = st.selectbox(
     "Type or select a movie from the dropdown",
     movie_list
 )
 
+# Display recommendations when the button is clicked
 if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+    # Display 5 recommended movies with posters side-by-side
     col1, col2, col3, col4, col5 = st.columns(5)
+
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
+
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
@@ -47,13 +72,11 @@ if st.button('Show Recommendation'):
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
+
     with col4:
         st.text(recommended_movie_names[3])
         st.image(recommended_movie_posters[3])
+
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
-
-
-
-
